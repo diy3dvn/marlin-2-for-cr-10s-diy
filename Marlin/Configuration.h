34e9adae69e1c5c6503c5251b9615c6adeee26a6
Nguyen Nhu Tuan <tnguyennhu@pentalog.com>
2021-02-28 15:59:36 +0700

    Config for SKR-1.3-20210228-for-RepRapDiscount-small

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2010,7 +2010,7 @@
 //
 // Note: Usually sold with a white PCB.
 //
-//#define REPRAP_DISCOUNT_SMART_CONTROLLER
+#define REPRAP_DISCOUNT_SMART_CONTROLLER
 
 //
 // GT2560 (YHCB2004) LCD Display
@@ -2168,7 +2168,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // https://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+// #define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // ReprapWorld Graphical LCD
